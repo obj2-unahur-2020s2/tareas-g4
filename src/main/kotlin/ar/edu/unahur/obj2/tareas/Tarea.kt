99a9a4ea9c40156cbf3297092aa64ad97030b540
NicoRamirez <nicolasmatiasramirez@gmail.com>
2020-11-04 22:04:43 -0300

    tareas

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,55 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+
+interface desarrolloDeTareas{
+    val responsable: Responsable
+    fun horasNecesarias():Int
+    fun costoDeTarea():Double
+    fun nomina():Int
+}
+
+class Tarea(val horasEstimadas:Int,override val responsable: Responsable,val costoInfraestructura:Double):desarrolloDeTareas{
+    var empleadosEnLaTarea = mutableListOf<Empleado>()
+    override fun nomina() =
+            (this.empleadosEnLaTarea + this.responsable).size
+    fun agregarEmpleadoAsignado(empleado: Empleado){
+        empleadosEnLaTarea.add(empleado)
+    }
+    override fun horasNecesarias() =
+            this.horasEstimadas / this.empleadosEnLaTarea.size
+   override fun costoDeTarea() =
+           (this.horasNecesarias()  * this.sumaDeTodosLosSueldosPorHora()) +
+                   (this.horasEstimadas * this.responsable.sueldoPorHora) + this.costoInfraestructura
+   fun sumaDeTodosLosSueldosPorHora() =
+           (this.empleadosEnLaTarea + this.responsable).sumBy { it.sueldoPorHora.toInt() }
+}
+class TareaIntegracion(override val responsable: Responsable):desarrolloDeTareas{
+    var subTareas = mutableListOf<Tarea>()
+    override fun horasNecesarias()=
+            subTareas.sumBy { it.horasNecesarias() } + this.horasDeReuniones()
+     fun horasDeReuniones()=
+             subTareas.sumBy { it.horasEstimadas } / 8
+    fun agregarTarea(tarea: Tarea){
+        subTareas.add(tarea)
+    }
+    override fun costoDeTarea()=
+            this.sumaTotalDeSubTareas().toDouble() + this.responsable.bonus(this)
+    fun sumaTotalDeSubTareas()=
+            subTareas.sumBy { it.costoDeTarea().toInt() }
+
+    override fun nomina()=
+            subTareas.sumBy { it.nomina() } + 1
+}
+
+interface trabajadores{
+
+
+    val sueldoPorHora:Double
+}
+ class Empleado(override val sueldoPorHora: Double):trabajadores{
+
+}
+class Responsable(override val sueldoPorHora: Double):trabajadores{
+    fun bonus(tareaIntegracion: TareaIntegracion) =
+            tareaIntegracion.sumaTotalDeSubTareas() * 0.03
+}
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -1,8 +1,48 @@
 package ar.edu.unahur.obj2.tareas
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
   describe("Una tarea") {
+      val responsable = Responsable(150.33)
+      val tarea1 = Tarea(13,responsable,150000.00)
+      val empleado1 = Empleado(99.50)
+      val empleado2 = Empleado(99.50)
+      val empleado3 = Empleado(99.50)
+      val tarea2 = Tarea(17,responsable,180000.00)
+      tarea1.agregarEmpleadoAsignado(empleado1)
+      tarea1.agregarEmpleadoAsignado(empleado2)
+      tarea1.agregarEmpleadoAsignado(empleado3)
+      tarea2.agregarEmpleadoAsignado(empleado1)
+      tarea2.agregarEmpleadoAsignado(empleado2)
+      val tareaIntegracion = TareaIntegracion(responsable)
+      tareaIntegracion.agregarTarea(tarea1)
+      tareaIntegracion.agregarTarea(tarea2)
+      it("saber la nomina de la tarea"){
+          tarea1.nomina().shouldBe(4)
+      }
+      it("saber cuantas horas se necesitan para finalizar la tarea"){
+          tarea1.horasNecesarias().shouldBe(4)
+      }
+      it("saber la suma total de todos los sueldos por hora"){
+          tarea1.sumaDeTodosLosSueldosPorHora().shouldBe(447)
+      }
+      it("costo de la tarea") {
+          tarea1.costoDeTarea().shouldBe(153742.29)
+      }
+      it("horas de reuniones para tareaIntegracion"){
+          tareaIntegracion.horasDeReuniones().shouldBe(3)
+      }
+      it("horas necesarias para tareaIntegracion"){
+          tareaIntegracion.horasNecesarias().shouldBe(15)
+      }
+      it("costo para tareaIntegracion"){
+          tareaIntegracion.costoDeTarea().shouldBe(349253.43)
+      }
+      it("nomina para tareaIntegracion"){
+          tareaIntegracion.nomina().shouldBe(8)
+      }
+
   }
 })
